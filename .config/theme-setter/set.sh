23b38b5e8a346f40244480c8770e4d031b0a0ae8
Youssef <youssef95elzein@gmail.com>
2022-05-11 18:02:00 -0700

    minor theme changes

--- .config/theme-setter/set.sh
+++ .config/theme-setter/set.sh
@@ -8,7 +8,7 @@ sed -i /feh.*/c\\"feh --bg-fill $rand_wall" "$berry/autostart"
 feh --bg-fill "$rand_wall"
 
 focus_color="$(jq -r ".colors.\"$1\".magenta" "$info")"
-unfocus_color="$(jq -r ".colors.\"$1\".gray" "$info")"
+unfocus_color="$(jq -r ".colors.\"$1\".cyan" "$info")"
 
 sed -i /.*" focus_color".*/c\\"berryc focus_color              $focus_color" "$berry/autostart"
 sed -i /.*" unfocus_color".*/c\\"berryc unfocus_color            $unfocus_color" "$berry/autostart"
