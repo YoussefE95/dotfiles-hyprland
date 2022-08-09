9762390da4e3f9936f0bd0ae4c0fb016d9ea9841
Youssef <youssef95elzein@gmail.com>
2022-08-09 12:25:58 -0700

    added bluetooth

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 yay -Syyu
 
-yay -S awesome-git alacritty zsh starship apple-fonts nerd-fonts-meslo ttf-meslo-nerd-font-powerlevel10k picom-ibhagwan-git ttf-raleway ranger neofetch cmatrix gotop-bin discord dropbox visual-studio-code-bin expressvpn spotify spicetify-cli libreoffice-fresh cmake extra-cmake-modules jq google-chrome gnome-keyring libsecret libgnome-keyring python-pip htop playerctl brightnessctl wmctrl xclip scrot cron ueberzug xorg-xinit polkit-gnome acpi ttf-font-awesome-5 lightdm lightdm-mini-greeter i3lock
+yay -S awesome-git alacritty zsh starship apple-fonts nerd-fonts-meslo ttf-meslo-nerd-font-powerlevel10k picom-ibhagwan-git ttf-raleway ranger neofetch cmatrix gotop-bin discord dropbox visual-studio-code-bin expressvpn spotify spicetify-cli libreoffice-fresh cmake extra-cmake-modules jq google-chrome gnome-keyring libsecret libgnome-keyring python-pip htop playerctl brightnessctl wmctrl xclip scrot cron ueberzug xorg-xinit polkit-gnome acpi ttf-font-awesome-5 lightdm lightdm-mini-greeter i3lock bluez bluez-utils blueman-git
 
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.zsh/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
@@ -16,6 +16,9 @@ sudo rm -rf ~/Documents ~/Music ~/Public ~/Videos ~/Templates ~/Pictures
 # set up cache for quote widget
 touch /home/younix/.cache/widget.quote
 touch /home/younix/.cache/widget.author
+
+# enable bluetooth
+sudo systemctl enable bluetooth
 
 # copy login screen files
 sudo cp lightdm.conf lightdm-mini-greeter.conf /etc/lightdm/
