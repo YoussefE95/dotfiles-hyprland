be9c0a88dae47c2ca89c642d47a82c99d9f2bddc
Youssef <youssef95elzein@gmail.com>
2022-11-29 01:09:32 -0800

    changed border color for unfocused windows

--- .config/theme-setter/templates/i3.sh
+++ .config/theme-setter/templates/i3.sh
@@ -6,10 +6,10 @@ dest_file="colors.conf"
 output="$(cat << THEME
 # class                 border  backgr. text    indicator child_border
 client.focused          #$(jq -r ".colors.\"$1\".green" "$2") #$(jq -r ".colors.\"$1\".background" "$2") #$(jq -r ".colors.\"$1\".foreground" "$2") #$(jq -r ".colors.\"$1\".blue" "$2")   #$(jq -r ".colors.\"$1\".green" "$2")
-client.focused_inactive #$(jq -r ".colors.\"$1\".gray" "$2") #$(jq -r ".colors.\"$1\".background" "$2") #$(jq -r ".colors.\"$1\".foreground" "$2") #$(jq -r ".colors.\"$1\".blue" "$2")   #$(jq -r ".colors.\"$1\".gray" "$2")
-client.unfocused        #$(jq -r ".colors.\"$1\".gray" "$2") #$(jq -r ".colors.\"$1\".background" "$2") #$(jq -r ".colors.\"$1\".foreground" "$2") #$(jq -r ".colors.\"$1\".blue" "$2")   #$(jq -r ".colors.\"$1\".gray" "$2")
-client.urgent           #$(jq -r ".colors.\"$1\".gray" "$2") #$(jq -r ".colors.\"$1\".background" "$2") #$(jq -r ".colors.\"$1\".foreground" "$2") #$(jq -r ".colors.\"$1\".blue" "$2")   #$(jq -r ".colors.\"$1\".gray" "$2")
-client.placeholder      #$(jq -r ".colors.\"$1\".gray" "$2") #$(jq -r ".colors.\"$1\".background" "$2") #$(jq -r ".colors.\"$1\".foreground" "$2") #$(jq -r ".colors.\"$1\".blue" "$2")   #$(jq -r ".colors.\"$1\".gray" "$2")
+client.focused_inactive #$(jq -r ".colors.\"$1\".background" "$2") #$(jq -r ".colors.\"$1\".background" "$2") #$(jq -r ".colors.\"$1\".foreground" "$2") #$(jq -r ".colors.\"$1\".blue" "$2")   #$(jq -r ".colors.\"$1\".background" "$2")
+client.unfocused        #$(jq -r ".colors.\"$1\".background" "$2") #$(jq -r ".colors.\"$1\".background" "$2") #$(jq -r ".colors.\"$1\".foreground" "$2") #$(jq -r ".colors.\"$1\".blue" "$2")   #$(jq -r ".colors.\"$1\".background" "$2")
+client.urgent           #$(jq -r ".colors.\"$1\".background" "$2") #$(jq -r ".colors.\"$1\".background" "$2") #$(jq -r ".colors.\"$1\".foreground" "$2") #$(jq -r ".colors.\"$1\".blue" "$2")   #$(jq -r ".colors.\"$1\".background" "$2")
+client.placeholder      #$(jq -r ".colors.\"$1\".background" "$2") #$(jq -r ".colors.\"$1\".background" "$2") #$(jq -r ".colors.\"$1\".foreground" "$2") #$(jq -r ".colors.\"$1\".blue" "$2")   #$(jq -r ".colors.\"$1\".background" "$2")
 
 client.background       #$(jq -r ".colors.\"$1\".background" "$2")
 THEME
