c2441512a5e8240605181b342ca3570db8116087
Youssef <youssef95elzein@gmail.com>
2023-07-16 18:16:38 -0700

    minor fixes

--- .config/theme-setter/templates/hypr.sh
+++ .config/theme-setter/templates/hypr.sh
@@ -1,8 +1,7 @@
 palette=("${@}")
 
-hyprctl keyword general:col.active_border "0xff${palette[5]}"
+hyprctl keyword general:col.active_border "0xff${palette[6]}"
 hyprctl keyword general:col.inactive_border "0xff${palette[11]}"
 hyprctl setcursor "${palette[12]}" 24
 
 swww img --transition-type wipe --transition-angle 30 --transition-step 60 "${palette[13]}"
-
