ea434c3d2ca6049718c34fcc517efd50428fcf3d
Youssef <youssef95elzein@gmail.com>
2023-07-18 15:11:54 -0700

    tweaked hypr and fixed beautiful discord

--- install.sh
+++ install.sh
@@ -1,27 +1,42 @@
-yay -Syyu
+# Update
+yay -Syu
 
-yay -S hyprland waybar-hyprland-git swww swaylock polkit libsecret polkit-gnome alacritty grim slurp dmenu dunst zsh neovim wl-clipboard starship apple-fonts ttf-iosevka-nerd ranger ueberzugpp discord slack-desktop dropbox expressvpn spotifyd spotify-tui cmake extra-cmake-modules jq python-pip htop brightnessctl acpi alsa bluez bluez-utils blueman-git pavucontrol nodejs npm
+# Install everything
+yay -S hyprland waybar-hyprland-git swww swaylock polkit libsecret polkit-gnome alacritty grim slurp dmenu dunst zsh neovim wl-clipboard starship apple-fonts ttf-iosevka-nerd ranger ueberzugpp discord slack-desktop dropbox expressvpn spotifyd spotify-tui cmake extra-cmake-modules bc jq python-pipx htop brightnessctl acpi alsa bluez bluez-utils blueman-git pavucontrol nodejs npm
 
+# Packer for Neovim
 git clone --depth 1 https://github.com/wbthomason/packer.nvim\
  ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.zsh/zsh-syntax-highlighting
-git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
+# Syntax highlighting for Zsh
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git\
+ ~/.zsh/zsh-syntax-highlighting
 
-python3 -m pip install -U https://github.com/leovoel/BeautifulDiscord/archive/master.zip
+# Auto suggestions for Zsh
+git clone https://github.com/zsh-users/zsh-autosuggestions\
+ ~/.zsh/zsh-autosuggestions
 
+# Beautiful Discord
+pipx install https://github.com/leovoel/BeautifulDiscord/archive/master.zip
+
+# Copy all config files
 sudo rsync -a .config .local .zshrc ~/
+
+# Copy commands for theme setter
 sudo cp .config/theme-setter/set-theme /usr/local/bin/
 sudo cp .config/theme-setter/get-theme /usr/local/bin/
 sudo cp .config/theme-setter/reapply-theme /usr/local/bin/
 
-sudo cp .config/spotifyd/spotifyd.service /etc/systemd/user/
-
+# Delete unused directories
 sudo rm -rf ~/Documents ~/Music ~/Public ~/Videos ~/Templates ~/Pictures
 
+# Copy Spotify Systemd service
+sudo cp .config/spotifyd/spotifyd.service /etc/systemd/user/
+
+# Enable daemons
 systemctl --user enable spotifyd.service --now
 sudo systemctl enable bluetooth
 sudo systemctl enable expressvpn
 
 # set discord theme
-# python3 -m beautifuldiscord --css ~/.config/beautdisc/global_theme.css
+# ~/.local/bin/beautifuldiscord --css ~/.config/beautdisc/global_theme.css
