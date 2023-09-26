cda2c6eb724a01d652f07e2409b7f0e86b4882f4
Youssef <youssef95elzein@gmail.com>
2023-09-26 10:21:37 -0700

    more minor updates

--- .config/hypr/dmenu.sh
+++ .config/hypr/dmenu.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-dmenu_run -b -fn 'HackNerdFont-13' -nb "#292828" -nf "#ddc7a1" -sb "#a9b665" -sf "#292828"
+dmenu_run -b -fn 'HackNerdFont-13' -nb "#f2e5bc" -nf "#4f3829" -sb "#6c782e" -sf "#f2e5bc"
