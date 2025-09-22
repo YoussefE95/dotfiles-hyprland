4d1ac7a42d84f3029fe5fb79ba6d90b92fd2615e
Youssef <youssef95elzein@gmail.com>
2025-09-22 14:33:24 -0700

    Changed quickshell theme setter

--- .config/theme-setter/setters/quickshell.sh
+++ .config/theme-setter/setters/quickshell.sh
@@ -2,7 +2,7 @@
 palette=("${@}")
 
 dest_dir="$HOME/.config/quickshell/"
-dest_file="Colors.qml"
+dest_file="Theme.qml"
 
 output="$(cat << THEME
 pragma Singleton
@@ -12,6 +12,10 @@ import Quickshell.Io
 import QtQuick
 
 Singleton {
+    property string fontFamily: "JetBrains Mono"
+    property int fontWeight: 500
+    property int fontSize: 13
+    property int iconSize: 16
     property color background: "#${palette[0]}"
     property color backgroundAlt: "#${palette[1]}"
     property color foreground: "#${palette[2]}"
