ffdda0b9bfdd4622d4171c45462fb29226a24259
Youssef <youssef95elzein@gmail.com>
2026-03-10 15:33:32 -0700

    Changed qt package and updated hypr conf

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ paru -Syu && paru -S \
     gwenview ttf-jetbrains-mono ttf-jetbrains-mono-nerd brightnessctl \
     jq python-pipx python-pillow docker docker-buildx ripgrep wl-clipboard \
     hyprpolkitagent xdg-desktop-portal-hyprland qt5-wayland qt6-wayland \
-    qt6ct-kde zsh zsh-autosuggestions zsh-history-substring-search \
+    qt6ct zsh zsh-autosuggestions zsh-history-substring-search \
     zsh-syntax-highlighting libappindicator-gtk3 archlinux-xdg-menu \
     xwayland-satellite playerctl pavucontrol-qt
 
